bc32ffaaf36ed1712e7973df2da147cd903bd264
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2024-12-15 17:21:12 +0300

    Start project

---
+++ src/test/java/kz/zhelezyaka/securitydemo/SecuritydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.securitydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritydemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
