49bf8782597a20dd2aefd4fac63cf29bda6a2d42
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2024-12-20 17:51:58 +0300

    Role Based Authorization

--- src/main/java/kz/zhelezyaka/securitydemo/controllers/DemoController.java
+++ src/main/java/kz/zhelezyaka/securitydemo/controllers/DemoController.java
@@ -1,13 +1,27 @@
 package kz.zhelezyaka.securitydemo.controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/test")
     public String testMapping() {
         return "This is a test mapping";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/user")
+    public String userEndpoint() {
+        return "Hello User";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String adminEndpoint() {
+        return "Hello admin";
     }
 }
