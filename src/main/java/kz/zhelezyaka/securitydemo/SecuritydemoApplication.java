ba677ee7015ab1cd6d83889b602c41f860d6d678
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2024-12-23 23:17:04 +0300

    Password Encoding and Hashing

--- src/main/java/kz/zhelezyaka/securitydemo/SecuritydemoApplication.java
+++ src/main/java/kz/zhelezyaka/securitydemo/SecuritydemoApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SecuritydemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SecuritydemoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SecuritydemoApplication.class, args);
+    }
 }
--- src/main/java/kz/zhelezyaka/securitydemo/config/SecurityConfig.java
+++ src/main/java/kz/zhelezyaka/securitydemo/config/SecurityConfig.java
@@ -1,16 +1,24 @@
 package kz.zhelezyaka.securitydemo.config;
 
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.*;
 
@@ -26,21 +34,40 @@ public class SecurityConfig {
         http.sessionManagement(session ->
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.httpBasic(withDefaults());
+        http.headers(headers ->
+                headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+        http.csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
+    public UserDetailsService userDetailsService(DataSource dataSource, PasswordEncoder passwordEncoder) {
         UserDetails user1 = User.withUsername("user1")
-                .password("{noop}password1")
+                .password(passwordEncoder.encode("password1"))
                 .roles("USER")
                 .build();
 
         UserDetails admin = User.withUsername("admin")
-                .password("{noop}password1")
+                .password(passwordEncoder.encode("password1"))
                 .roles("ADMIN")
                 .build();
 
-        return new InMemoryUserDetailsManager(user1, admin);
+        JdbcUserDetailsManager userDetailsManager =
+                new JdbcUserDetailsManager(dataSource);
+
+        if (!userDetailsManager.userExists("user1")) {
+            userDetailsManager.createUser(user1);
+        }
+
+        if (!userDetailsManager.userExists("admin")) {
+            userDetailsManager.createUser(admin);
+        }
+
+        return userDetailsManager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
